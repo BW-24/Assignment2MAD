c1a36704358aaf6f968f84a293b6de489968cccd
yuph0ric <mporyang@gmail.com>
2025-10-13 23:29:06 +0800

    added LibraryViewModel.kt

---
+++ app/src/main/java/com/example/pocketlibrary/LibraryViewModel.kt
@@ -0,0 +1,64 @@
+package com.example.pocketlibrary
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.util.query
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+// this is for the offline local storage and search
+data class LibraryUiState(
+    val faves: List<FavouriteBook> = emptyList(),
+    val query: String = ""
+)
+
+class LibraryViewModel(private val dao: FavouriteBookDAO): ViewModel() {
+    private val _state = MutableStateFlow(LibraryUiState())
+    val state: MutableStateFlow<LibraryUiState> = _state
+
+    init {
+        load()
+    }
+
+    fun load() {
+        viewModelScope.launch {
+            _state.value = _state.value.copy(faves = dao.getAll())
+        }
+    }
+
+    fun addBook(book: FavouriteBook) {
+        viewModelScope.launch {
+            dao.insert(book)
+            load()
+        }
+    }
+
+    fun removeBook(book: FavouriteBook) {
+        viewModelScope.launch {
+            dao.delete(book)
+            load()
+        }
+    }
+
+    fun updateBook(book: FavouriteBook) {
+        viewModelScope.launch {
+            dao.update(book)
+            load()
+        }
+    }
+
+    fun updateQuery(q: String) {
+        _state.value = _state.value.copy(query = q)
+        viewModelScope.launch {
+            if(q.isEmpty()) {
+                load()
+            }
+            else {
+                _state.value= _state.value.copy(faves = dao.search(q))
+            }
+        }
+    }
+}
+
