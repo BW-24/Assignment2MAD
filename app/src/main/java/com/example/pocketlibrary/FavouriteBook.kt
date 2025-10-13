185b9edea995b6376104d59e47fc56bde39b3854
yuph0ric <mporyang@gmail.com>
2025-10-13 22:13:43 +0800

    added FavouriteBook data class

---
+++ app/src/main/java/com/example/pocketlibrary/FavouriteBook.kt
@@ -0,0 +1,20 @@
+package com.example.pocketlibrary
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favourite_books")
+data class FavouriteBook (
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "author")
+    val author: String?,
+    @ColumnInfo(name = "age")
+    val year: Int?,
+    @ColumnInfo(name = "cover")
+    val cover: Int,
+
+)
